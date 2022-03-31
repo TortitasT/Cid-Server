399a73b8860632895a86ddc479c1d1ba6b8ea8b3
Tortitas <victorgf2011@gmail.com>
2022-03-31 12:27:43 +0200

    node server test

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-const port = process.env.PORT || 28962
-const io = require('socket.io')(port)
+const {Server} = require('socket.io')
 
-console.log(`Server listening on port ${port}`)
+const port = process.env.PORT || 28962
+
+const io = new Server()
+
+io.on('connection', (socket) => {
+  const client = socket.handshake.address
+  console.log(`Player connected from ip: ${client.address}:${client.port}`)
+})
+io.on('disconnect', () => {
+  console.log(`Player disconnected`)
+})
+io.on('connect_error', (err) => {
+  console.log(`connect_error due to ${err.message}`)
+})
+
+io.listen(port)
