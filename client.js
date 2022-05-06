265928007aa5d917465d8da9756f1e679c2f4189
Tortitas <victorgf2011@gmail.com>
2022-05-06 14:00:20 +0200

    GUI compatibility, disabled walk mock

--- client.js
+++ client.js
@@ -1,86 +1,86 @@
-const { io } = require("socket.io-client");
-const prompt = require("prompt-sync")();
-const ip = process.env.IP || "localhost";
-const port = process.env.PORT || 28962;
+const { io } = require('socket.io-client')
+const prompt = require('prompt-sync')()
+const ip = process.env.IP || 'localhost'
+const port = process.env.PORT || 28962
 
 const character = {
-  name: prompt("Character name: "),
-  level: prompt("Character level: "),
-};
+	name: prompt('Character name: '),
+	level: prompt('Character level: '),
+}
 const pos = {
-  x: 0,
-  y: 0,
-};
-let id = 0;
+	x: 0,
+	y: 0,
+}
+let id = 0
 
 // List of players connected and their info
-let players = [];
+let players = []
 
 // Gets a player from the above list
 function getPlayer(id) {
-  for (let i = 0; i < players.length; i++) {
-    if (players[i].id === id) {
-      return players[i];
-    }
-  }
+	for (let i = 0; i < players.length; i++) {
+		if (players[i].id === id) {
+			return players[i]
+		}
+	}
 }
 
-const socket = io(`ws://${ip}:${port}/`);
+const socket = io(`ws://${ip}:${port}/`)
 
-console.log(`Trying to connect to ${ip}:${port}`);
+console.log(`Trying to connect to ${ip}:${port}`)
 
-socket.on("connect", () => {
-  console.clear();
-  console.log("Connected as:");
-  console.log(socket.id);
+socket.on('connect', () => {
+	console.clear()
+	console.log('Connected as:')
+	console.log(socket.id)
 
-  id = socket.id;
-  socket.emit("config", { id: socket.id, character: character, pos: pos });
+	id = socket.id
+	socket.emit('config', { id: socket.id, character: character, pos: pos })
 
-  // Walk mock
-  setTimeout(() => {
-    setInterval(() => {
-      pos.x++;
-      socket.emit("update", { pos: pos });
-    }, 1000);
-  }, 500);
+	// Walk mock
+	// setTimeout(() => {
+	//   setInterval(() => {
+	//     pos.x++;
+	//     socket.emit("update", { pos: pos });
+	//   }, 1000);
+	// }, 500);
 
-  socket.on("currentPlayers", (response) => {
-    players = response.players;
+	socket.on('currentPlayers', (response) => {
+		players = response.players
 
-    console.log(players);
-  });
+		console.log(players)
+	})
 
-  socket.on("registered", (response) => {
-    players.push(response.player);
-    console.log(`Player ${response.player.character.name} connected`);
+	socket.on('registered', (response) => {
+		players.push(response.player)
+		console.log(`Player ${response.player.character.name} connected`)
 
-    console.log(players);
-  });
+		console.log(players)
+	})
 
-  socket.on("updated", (response) => {
-    getPlayer(response.id).pos = response.pos;
-  });
+	socket.on('updated', (response) => {
+		getPlayer(response.id).pos = response.pos
+	})
 
-  socket.on("disconnected", (response) => {
-    // console.log(`Updated user: ${response.id}`);
-    players.splice(players.indexOf(getPlayer(response.id)), 1);
+	socket.on('disconnected', (response) => {
+		// console.log(`Updated user: ${response.id}`);
+		players.splice(players.indexOf(getPlayer(response.id)), 1)
 
-    console.log(`Player ${response.name} disconnected`);
-    console.log(players);
-  });
+		console.log(`Player ${response.name} disconnected`)
+		console.log(players)
+	})
 
-  socket.on("connect_error", (err) => {
-    console.log(`Connection error: ${err.message}`);
-  });
-});
+	socket.on('connect_error', (err) => {
+		console.log(`Connection error: ${err.message}`)
+	})
+})
 
 setInterval(() => {
-  for (let i = 0; i < players.length; i++) {
-    console.log(`id: ${players[i].id}`);
-    console.log(`name: ${players[i].character.name}`);
-    console.log(`level: ${players[i].character.level}`);
-    console.log(`pos: ${players[i].pos.x}, ${players[i].pos.y}`);
-    console.log(`\n`);
-  }
-}, 1000);
+	for (let i = 0; i < players.length; i++) {
+		console.log(`id: ${players[i].id}`)
+		console.log(`name: ${players[i].character.name}`)
+		console.log(`level: ${players[i].character.level}`)
+		console.log(`pos: ${players[i].pos.x}, ${players[i].pos.y}`)
+		console.log(`\n`)
+	}
+}, 1000)
--- server.js
+++ server.js
@@ -1,100 +1,103 @@
-const { Server } = require("socket.io");
-const stdin = process.openStdin();
-const Chalk = require("chalk");
-const Player = require("./classes/player.js");
-const Vector2 = require("./classes/vector2.js");
-const input = require("./classes/commands.js");
-require("dotenv").config();
+const { Server } = require('socket.io')
+const stdin = process.openStdin()
+const Chalk = require('chalk')
+const Player = require('./classes/player.js')
+const Vector2 = require('./classes/vector2.js')
+const input = require('./classes/commands.js')
+require('dotenv').config()
 
 // Socket io configuration
-const io = new Server();
+const io = new Server()
 
 // Configs
 const configs = {
-  port: process.env.PORT || 28962,
-  players: [],
-  max_players: process.env.MAXPLAYERS || 4,
-  log: Chalk.blue("Start of log\n"),
-};
+	port: process.env.PORT || 28962,
+	players: [],
+	max_players: process.env.MAXPLAYERS || 10,
+	log: Chalk.blue('Start of log\n'),
+}
+exports.configs = configs
 
 // Prints text on screen and logs it
 function print(text) {
-  const currentDate = new Date();
+	const currentDate = new Date()
 
-  configs.log += `[${Intl.DateTimeFormat("en-US", { weekday: "long" }).format(
-    currentDate.getDay()
-  )} ${currentDate.getDate()}/${currentDate.getMonth()}/${currentDate.getFullYear()} ${currentDate.getHours()}:${currentDate.getMinutes()}] ${text}\n`;
-  console.log(text);
+	configs.log += `[${Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(
+		currentDate.getDay()
+	)} ${currentDate.getDate()}/${currentDate.getMonth()}/${currentDate.getFullYear()} ${currentDate.getHours()}:${currentDate.getMinutes()}] ${text}\n`
+	console.log(text)
 }
 
 // Gets a player from the db
 function getPlayer(id) {
-  for (let i = 0; i < configs.players.length; i++) {
-    if (configs.players[i].id === id) {
-      return configs.players[i];
-    }
-  }
+	for (let i = 0; i < configs.players.length; i++) {
+		if (configs.players[i].id === id) {
+			return configs.players[i]
+		}
+	}
 }
 
 // Connection logic
-io.on("connection", (socket) => {
-  // If server is full disconnect
-  if (configs.players.length >= configs.max_players) {
-    socket.emit("full");
-    socket.disconnect();
-    print(Chalk.red("Player tried to join but server is full"));
-    return;
-  }
+io.on('connection', (socket) => {
+	// If server is full disconnect
+	if (configs.players.length >= configs.max_players) {
+		socket.emit('full')
+		socket.disconnect()
+		print(Chalk.red('Player tried to join but server is full'))
+		return
+	}
 
-  // Receive player character information
-  socket.on("config", (response) => {
-    newPlayer = new Player(response);
-    print(
-      Chalk.cyan(
-        `Player ${newPlayer.character.name} (${socket.id}) connected from ip: ${socket.conn.remoteAddress}`
-      )
-    );
+	// Receive player character information
+	socket.on('config', (response) => {
+		newPlayer = new Player(response)
+		newPlayer.ip = socket.conn.remoteAddress
 
-    // Send the current players to the new player and tell the rest that a new player has joined
-    socket.emit("currentPlayers", { players: configs.players });
-    socket.broadcast.emit("registered", { player: newPlayer });
+		print(
+			Chalk.cyan(
+				`Player ${newPlayer.character.name} (${socket.id}) connected from ip: ${newPlayer.ip}`
+			)
+		)
 
-    // Add player to db
-    configs.players.push(newPlayer);
-  });
+		// Send the current players to the new player and tell the rest that a new player has joined
+		socket.emit('currentPlayers', { players: configs.players })
+		socket.broadcast.emit('registered', { player: newPlayer })
 
-  // Update on tick
-  socket.on("update", (response) => {
-    const player = getPlayer(socket.id);
+		// Add player to db
+		configs.players.push(newPlayer)
+	})
 
-    if (player) {
-      const distance = Vector2.distance(player?.pos, response.pos);
+	// Update on tick
+	socket.on('update', (response) => {
+		const player = getPlayer(socket.id)
 
-      // Check if player has moved
-      if (distance > 0) {
-        player.pos = response.pos;
-        socket.broadcast.emit("updated", { id: player.id, pos: player.pos });
-      }
-    }
-  });
+		if (player) {
+			const distance = Vector2.distance(player?.pos, response.pos)
 
-  // Manage disconnection of players
-  socket.on("disconnect", () => {
-    const name = getPlayer(socket.id)?.character.name || "Unknown";
-    configs.players.splice(configs.players.indexOf(getPlayer(socket.id)), 1);
-    print(Chalk.yellow(`Player ${name} (${socket.id}) disconnected`));
+			// Check if player has moved
+			if (distance > 0) {
+				player.pos = response.pos
+				socket.broadcast.emit('updated', { id: player.id, pos: player.pos })
+			}
+		}
+	})
 
-    socket.broadcast.emit("disconnected", { id: socket.id, name: name });
-  });
-});
+	// Manage disconnection of players
+	socket.on('disconnect', () => {
+		const name = getPlayer(socket.id)?.character.name || 'Unknown'
+		configs.players.splice(configs.players.indexOf(getPlayer(socket.id)), 1)
+		print(Chalk.yellow(`Player ${name} (${socket.id}) disconnected`))
+
+		socket.broadcast.emit('disconnected', { id: socket.id, name: name })
+	})
+})
 
 // Listen for commands
-stdin.addListener("data", function (d) {
-  input(d.toString().trim().toLowerCase(), configs);
-});
+stdin.addListener('data', function (d) {
+	input(d.toString().trim().toLowerCase(), configs)
+})
 
 // Listen port
-io.listen(configs.port);
+io.listen(configs.port)
 
 // Print status on launch
-input("status", configs);
+input('status', configs)
